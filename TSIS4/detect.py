bc7367b296389640b168b16f3b60e07976923dab
Adilbek-Asilzat <78736705+Adilbek-Asilzat@users.noreply.github.com>
2021-04-13 13:47:43 +0600

    Add files via upload

---
+++ TSIS4/detect.py
@@ -0,0 +1,13 @@
+#Detect Floating Point Number
+count=int(input().strip())
+for _ in range(count):
+    ans=False
+    try:
+        string=input().strip()
+        number=float(string)
+        ans=True
+        number=int(string)
+        ans=False
+    except:
+        pass
+    print(ans)
---
+++ TSIS4/valid.py
@@ -0,0 +1,5 @@
+#Validating phone numbers
+if re.match(r'[789]\d{9}$',raw_input()):   
+    print 'YES'  
+else:  
+    print 'NO'
