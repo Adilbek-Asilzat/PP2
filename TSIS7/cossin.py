5de06c23dc9ff0319764ce0186b02c3dcd70de1b
Adilbek-Asilzat <78736705+Adilbek-Asilzat@users.noreply.github.com>
2021-04-13 13:15:03 +0600

    Add files via upload

---
+++ TSIS7/cossin.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,4*np.pi,0.1)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+x = np.arange(0,4*np.pi,0.1)
+y = np.sin(x)
+z = np.cos(x)
+
+plt.plot(x,y,z)
+plt.show()
+
+x = np.arange(0,4*np.pi,0.1)
+y = np.sin(x)
+z = np.cos(x)
+
+plt.plot(x,y,x,z)
+plt.legend(['sin(x)',['cos(x)']])
+plt.show()
+
+
